3cce2c6d79ceab7c21631553135522e926593ed0
KevinBehlmaier <kevin.behlmaier@gmail.com>
2018-04-10 16:55:22 -0600

    Adds mouse dragging ability to canvas

--- designs.js
+++ designs.js
@@ -7,7 +7,7 @@ var size = document.getElementById('sizePicker');
 var pixelCanvas = document.getElementById('pixelCanvas');
 var color = document.getElementById('colorPicker');
 
-color.addEventListener("click", function(){});
+
 
 function makeGrid() {
   var height = document.getElementById('inputHeight').value;
@@ -21,15 +21,15 @@ function makeGrid() {
     for (var j = 0; j < width; j++) {
       var td = document.createElement('td')
       tr.appendChild(td)
-      td.addEventListener("click", fillSquare)
+      td.addEventListener("mousedown", fillSquare)
   	}
 
   }
   // Your code goes here!
 };
 
-
-
 function fillSquare () {
     this.setAttribute("style", `background-color: ${color.value}`);
 }
+
+//color.addEventListener("click", function(){});   maybe dont need
